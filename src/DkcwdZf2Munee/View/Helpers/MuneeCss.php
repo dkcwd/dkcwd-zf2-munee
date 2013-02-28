a411ac997becc72c87e8628319d8d3bc8dd52b65
Dave Clark <dave@dkcwd.com.au>
2013-02-28 14:16:13 +1100

    Updated MuneeCss
    Discussed functionality with Cody and Ollie and updated view helper with
    3rd param to provide an option for returning just the 'munified' path
    without an element wrapper.  Cody contributed the revised code
    significantly improving the readability.

--- src/DkcwdZf2Munee/View/Helpers/MuneeCss.php
+++ src/DkcwdZf2Munee/View/Helpers/MuneeCss.php
@@ -13,31 +13,38 @@ use Zend\View\Helper\AbstractHelper;
 class MuneeCss extends AbstractHelper
 {
     /**
-     * View helper to format strings for making requests to the Munee Controller via the 
+     * View helper to format strings for making requests to the Munee Controller via the
      * custom route provided in this module.
-     * 
-     * @param array $array An array containing strings representing paths to css files.
-     * @param bool $minify [optional] a switch to disable css file minification
-     * @return A formatted string or null if invalid parameters are supplied
+     *
+     * @param array $files An array containing strings representing paths to css files.
+     * @param bool $minify [optional] a switch to disable js file minification
+     * @param bool $returnElementWrapper [optional] a switch to prevent wrapping the path
+     * returned within an element wrapper
+     * @return string|null A formatted string or null if invalid parameters are supplied
      */
-    public function __invoke($array, $minify = true)
-    {   
-        if (! is_array($array)) return;
-        $minifyValue = ($minify == false) ? 'false' : 'true';
-        
-        $string = '';
-        $tempArray = array();
-        
-        foreach ($array as $item) {
-            if (! is_string($item)) continue;
-            $tempArray[] = $item;
-        }      
-        
-        if (count($tempArray) == 0) return; 
-                
-        $string = implode(',', $tempArray);
-        unset($tempArray);
-        
-        return '<link rel="stylesheet" href="/munee?files=' . $string . '&minify=' . $minifyValue .'">' . PHP_EOL;
+    public function __invoke($files, $minify = true, $returnElementWrapper = true)
+    {
+        if (! is_array($files)) {
+            return null;
+        }
+    
+        foreach ($files as $k => $item) {
+            if (! is_string($item)) {
+                unset($files[$k]);
+            }
+        }
+    
+        if (empty($files)) {
+            return null;
+        }
+    
+        $minifyValue = ($minify === false) ? 'false' : 'true';
+        $src = sprintf('/munee?files=%s&minify=%s', implode(',', $files), $minifyValue);
+    
+        if ((boolean) $returnElementWrapper) {
+            return sprintf('<link rel="stylesheet" href="%s">', $src) . PHP_EOL;
+        } else {
+            return $src . PHP_EOL;
+        }
     }
 }
